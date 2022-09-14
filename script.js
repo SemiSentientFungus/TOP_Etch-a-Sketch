bf982ab426af3673e2db62608ba4c520b3e06e30
Semm <semisentientfungus@gmail.com>
2022-09-14 12:59:20 +0200

    add etch-a-sketch black function

--- script.js
+++ script.js
@@ -1,12 +1,28 @@
 const container = document.getElementById("container");
+makeRows(16, 16);
+
+const grid = document.querySelectorAll('.grid-item');
+
+console.log(grid);
+for (const box of grid) {
+  box.addEventListener('mouseover', function onClick() {
+    box.setAttribute('style', 'background: black');
+  });
+}
+
+
+
 
 function makeRows(rows, cols) {
   container.style.setProperty('--grid-rows', rows);
   container.style.setProperty('--grid-cols', cols);
   for (c = 0; c < (rows * cols); c++) {
     let cell = document.createElement("div");
-    container.appendChild(cell).className = "grid-item";
+    container.appendChild(cell).className = 'grid-item';
+    /*container.innerHTML += '<div class="grid-item" onmouseover="colour()"></div>'*/
   };
 };
 
-makeRows(16, 16);
+function colour() {
+  console.log("grid");
+}
